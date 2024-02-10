7de87661d3b41dbfc2a9d06b004641dcad50b3c6
Sangwoo Shin <excorp79@gmail.com>
2024-02-10 18:39:53 +0900

    Revert "Change how blacklist/whitelist is handled"
    This reverts commit b2ed243ea0ba16a2bb0ebd0779ae7183b5e430fc.

--- Modules/Debuffs.lua
+++ Modules/Debuffs.lua
@@ -29,14 +29,8 @@ local IsForbidden = IsForbidden
 local next = next
 
 local frame_registry = {}
--- local org_SpellGetVisibilityInfo
-local module_enabled
-local blacklist = {}
-local whitelist = {}
 
 function Debuffs:OnEnable()
-    module_enabled = true
-
     local debuffColors = {
         Curse   = { r = 0.6, g = 0.0, b = 1.0 },
         Disease = { r = 0.6, g = 0.4, b = 0.0 },
@@ -72,16 +66,12 @@ function Debuffs:OnEnable()
     stackOpt.point = addon:ConvertDbNumberToPosition(stackOpt.point)
     stackOpt.relativePoint = addon:ConvertDbNumberToPosition(stackOpt.relativePoint)
     --blacklist
-    for k in pairs(blacklist) do
-        blacklist[k] = nil
-    end
+    local blacklist = {}
     for spellId, value in pairs(addon.db.profile.Debuffs.Blacklist) do
         blacklist[tonumber(spellId)] = true
     end
     --whitelist
-    for k in pairs(whitelist) do
-        whitelist[k] = nil
-    end
+    local whitelist = {}
     for spellId, value in pairs(addon.db.profile.Debuffs.Whitelist) do
         whitelist[tonumber(spellId)] = value
     end
@@ -143,38 +133,6 @@ function Debuffs:OnEnable()
     local point = addon:ConvertDbNumberToPosition(frameOpt.point)
     local relativePoint = addon:ConvertDbNumberToPosition(frameOpt.relativePoint)
     local followPoint, followRelativePoint = addon:GetAuraGrowthOrientationPoints(frameOpt.orientation)
-
-    if not AuraUtil.org_ShouldDisplayDebuff then
-        AuraUtil.org_ShouldDisplayDebuff = AuraUtil.ShouldDisplayDebuff
-        AuraUtil.ShouldDisplayDebuff = function(unitCaster, spellId)
-            DevTool:AddData({unitCaster, spellId, canApplyAura}, "not enabled ShouldDisplayDebuff()")
-
-            if module_enabled then
-                DevTool:AddData({unitCaster, spellId, canApplyAura}, "ShouldDisplayDebuff()")
-                if blacklist[spellId] then
-                    return false
-                elseif whitelist[spellId] then
-                    return true
-                end
-            end
-            return AuraUtil.org_ShouldDisplayDebuff(unitCaster, spellId, canApplyAura)
-        end
-    end
-    --[[
-    if not org_SpellGetVisibilityInfo then
-        org_SpellGetVisibilityInfo = SpellGetVisibilityInfo
-        SpellGetVisibilityInfo = function(spellId, visType)
-            if module_enabled then
-                if blacklist[spellId] then
-                    return true, false, false
-                elseif whitelist[spellId] then
-                    return false
-                end
-            end
-            return org_SpellGetVisibilityInfo(spellId, visType)
-        end
-    end
-    ]]
 
     local onSetDeuff = function(debuffFrame, aura)
         if debuffFrame:IsForbidden() then --not sure if this is still neede but when i created it at the start if dragonflight it was
@@ -250,11 +208,9 @@ function Debuffs:OnEnable()
         -- set placed aura / other aura
         local frameNum = 1
         frame.debuffs:Iterate(function(auraInstanceID, aura)
-            --[[
             if blacklist[aura.spellId] then
                 return false
             end
-            ]]
 
             if userPlaced[aura.spellId] then
                 local idx = frame_registry[frame].placedAuraStart + userPlaced[aura.spellId].idx - 1
@@ -301,7 +257,6 @@ function Debuffs:OnEnable()
     end
     self:HookFunc("CompactUnitFrame_HideAllDebuffs", onHideAllDebuffs)
 
-    --[[
     local function onUpdateAuras(frame, unitAuraUpdateInfo)
         if not frame_registry[frame] or not frame.debuffs then
             return
@@ -353,7 +308,6 @@ function Debuffs:OnEnable()
         end
     end
     self:HookFunc("CompactUnitFrame_UpdateAuras", onUpdateAuras)
-    ]]
 
     local function onFrameSetup(frame)
         if frame.maxDebuffs == 0 then
@@ -502,12 +456,11 @@ function Debuffs:OnEnable()
             end
         end
     end)
+
 end
 
 --parts of this code are from FrameXML/CompactUnitFrame.lua
 function Debuffs:OnDisable()
-    module_enabled = false
-
     self:DisableHooks()
     local restoreDebuffFrames = function(frame)
         if frame_registry[frame] then

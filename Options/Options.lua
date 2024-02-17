58f7eca7b9d737eafa7cc39d9ca6273fd66d8660
Sangwoo Shin <excorp79@gmail.com>
2024-02-17 22:32:19 +0900

    Enable AuraHighlight for classic

--- Options/Options.lua
+++ Options/Options.lua
@@ -255,7 +255,6 @@ local options = {
                             set = "SetModuleStatus",
                         },
                         AuraHighlight = {
-                            hidden = not isRetail,
                             order = 8,
                             type = "toggle",
                             name = "Aura Highlight",
@@ -1375,7 +1374,7 @@ local options = {
             order = 5,
             name = "Aura Highlight",
             type = "group",
-            hidden = not isRetail or AuraHighlight_disabled,
+            hidden = AuraHighlight_disabled,
             args = {
                 Config = {
                     order = 1,

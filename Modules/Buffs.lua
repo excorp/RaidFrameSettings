caf33ec44df093e3c819c25519fe65f2a8e8d4f6
Sangwoo Shin <excorp79@gmail.com>
2024-04-14 21:18:06 +0900

    Remove test code

--- Modules/Buffs.lua
+++ Modules/Buffs.lua
@@ -442,7 +442,6 @@ function Buffs:OnEnable()
                         buffFrame:SetPoint(v.point, frame, v.relativePoint, v.xOffset - x / 2, v.yOffset + y / 2)
                     end
                 end
-                DevTool:AddData({ frame_registry[frame], frame_registry[frame].auraGroupEnd[groupNo] or "no end", frame_registry[frame].auraGroupStart[groupNo] or "no start", groupNo })
                 local groupSize = frame_registry[frame].auraGroupEnd[groupNo] - frame_registry[frame].auraGroupStart[groupNo] + 1
                 for i = groupFrameNum[groupNo] or 1, groupSize do
                     local idx = frame_registry[frame].auraGroupStart[groupNo] + i - 1
